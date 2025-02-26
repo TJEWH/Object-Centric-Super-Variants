bae6a1a0f07f66f09cd622bc315cd9dc6ae5eeb7
Tim Edelmann <tim.edelmann@campus.lmu.de>
2025-02-26 16:43:39 +0100

    grouped, formatted and renamed logs, replaced wrong p2p log

--- config.py
+++ config.py
@@ -1,38 +1,50 @@
-__test_logs = [
-    "EventLogs/BPI2017-Top10.jsonocel",
-    "EventLogs/p2p.jsonocel",
-    "EventLogs/test_log.jsonocel",
-    "EventLogs/Presentation_Example.jsonocel"]
+__event_dir = "EventLogs/"
+__expl_dir = __event_dir + "0_example/"
+__full_dir = __event_dir + "1_full/"
+__eval_dir = __event_dir + "2_evaluation/"
+
+__logs = [
+    __expl_dir + "ocel.jsonocel",
+    __expl_dir + "presentation.jsonocel",
+    __expl_dir + "thesis.jsonocel",
+
+    __full_dir + "BPI2017-Top10.jsonocel",  # 3
+    __full_dir + "running-example.jsonocel",
+    __full_dir + "test_log.jsonocel",  # 5
+
+    __eval_dir + "order_process.jsonocel",
+    __eval_dir + "p2p.jsonocel",  # 7
+]
+
+TEST_LOGS_3 = [__logs[0], __logs[1], __logs[2]]
+BPI_2017_TOP10_LOG = __logs[3]  # Mostly used for evaluation
+P2P_LOG = __logs[7]  # Publication Log
+
 
 __branching = {
     "main": {
         "mode": 8,
-        "file": __test_logs[0],
+        "file": __logs[3],
         "parameters": {"execution_extraction": "leading_type"},
         "alignment": {"align": False, "repeat": None},
     },
     "expl": {
         "mode": None,
-        "file": __test_logs[1],
+        "file": __logs[7],
         "alignment": {"align": True, "repeat": True},
     },
     "eval": {
         "mode": 9,
-        "file": __test_logs[1],
-        "file_eval": __test_logs[0],
+        "file": __logs[7],
+        "file_eval": __logs[3],
         "alignment": {"align": False, "repeat": None},
     },
     "publ": {
         "mode": 9,
-        "file": __test_logs[2],
-        "file_eval": __test_logs[1],
+        "file": __logs[5],
+        "file_eval": __logs[7],
         "alignment": {"align": True, "repeat": True},
     },
 }
 
 BRANCH = __branching["publ"]
-
-EVALUATION_LOG = __test_logs[0]
-PUBLICATION_LOG = __test_logs[1]
-TEST_LOG = __test_logs[3]
-PRESENTATION_LOG = __test_logs[2]
