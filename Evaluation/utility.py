caa0afbdbcba765b9017c692063a8a46bfa74822
Tim Edelmann <tim.edelmann@campus.lmu.de>
2025-04-21 15:32:01 +0200

    added utility function for viz steps in between

--- Evaluation/utility.py
+++ Evaluation/utility.py
@@ -1,6 +1,10 @@
+from ocpa.visualization.log.variants import factory as variants_visualization_factory
+
 import Inter_Variant_Generation as IEVG
 import Super_Variant_Definition as SVD
 import Super_Variant_Hierarchy as SVH
+import Super_Variant_Visualization as SVV
+import Input_Extraction_Definition as IED
 
 
 def build_triad_hierarchy(initial_super_variants):
@@ -12,7 +16,7 @@ def build_triad_hierarchy(initial_super_variants):
     for hierarchy in hierarchies:
         SVH.explore_hierarchy_bottom_up(hierarchy)
 
-    print_hierarchy_costs(hierarchies)
+    # print_hierarchy_costs(hierarchies)
 
 
 def print_hierarchy_costs(hierarchies):
@@ -56,3 +60,44 @@ def manual_alignment_l(lane, positions):
                 lane.elements[j].choices[h] = manual_alignment_l(lane.elements[j].choices[h], positions[j][2][h])
 
     return lane
+
+
+def visualize_variant_summarization_step(ocel):
+    # Visualize input variants
+    variant_layout = variants_visualization_factory.apply(ocel)
+    variants = []
+
+    number_of_variants = len(ocel.variants)
+
+    _range = range(10) if number_of_variants > 10 else range(number_of_variants)
+
+    for idx in _range:
+        extracted_variant = IED.extract_lanes(
+            variant=variant_layout[ocel.variants[idx]],
+            frequency=ocel.variant_frequencies[idx]
+        )
+
+        variants.append(extracted_variant)
+        SVV.visualize_variant(extracted_variant, idx)
+
+
+def visualize_intra_summarizations(intra_variant_summarizations, all_summarizations, use_artificial_frequencies=False):
+    try:
+        # Visualize selected intra-variant summarizations
+        global_frequencies = [0.069, 0.05, 0.047, 0.03, 0.025, 0.019, 0.019, 0.018, 0.017, 0.016]
+        for i in range(len(intra_variant_summarizations)):
+            # Replace with artificial frequencies
+            if use_artificial_frequencies:
+                intra_variant_summarizations[i].frequency = global_frequencies[i]
+            # Visualize the summarization
+            SVV.visualize_super_variant(intra_variant_summarizations[i])
+
+        # Visualize intra-variant summarizations of variant v_3
+        SVV.MARK_INCORRECT_INTERACTIONS = False
+        SVV.visualize_variant(all_summarizations[3][1][1][0], 3)
+        SVV.visualize_variant(all_summarizations[4][1][1][0], 3)
+        SVV.MARK_INCORRECT_INTERACTIONS = True
+
+    except:
+        for x in intra_variant_summarizations:
+            print(x)
