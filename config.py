62091b5231af5626e38cd0c41a9b004ae10b6f1a
Tim Edelmann <tim.edelmann@campus.lmu.de>
2025-04-21 15:27:34 +0200

    removed duplicate log, updated config with log registration

--- config.py
+++ config.py
@@ -6,43 +6,43 @@ __eval_dir = __event_dir + "2_evaluation/"
 __logs = [
     __expl_dir + "ocel.jsonocel",
     __expl_dir + "presentation.jsonocel",
-    __expl_dir + "thesis.jsonocel",
 
-    __full_dir + "BPI2017-Top10.jsonocel",  # 3
+    __full_dir + "BPI2017-Top10.jsonocel",  # 2
     __full_dir + "running-example.jsonocel",
-    __full_dir + "test_log.jsonocel",  # 5
+    __full_dir + "test_log.jsonocel",  # 4
 
     __eval_dir + "order_process.jsonocel",
-    __eval_dir + "p2p.jsonocel",  # 7
+    __eval_dir + "p2p.jsonocel",  # 6
 ]
 
-TEST_LOGS_3 = [__logs[0], __logs[1], __logs[2]]
-BPI_2017_TOP10_LOG = __logs[3]  # Mostly used for evaluation
-P2P_LOG = __logs[7]  # Publication Log
+TEST_LOG_1 = __logs[0]
+TEST_LOG_2 = __logs[1]
+BPI_2017_TOP10_LOG = __logs[2]  # Mostly used for evaluation
+P2P_LOG = __logs[6]  # Publication Log
 
 
 __branching = {
     "main": {
         "mode": 8,
-        "file": __logs[3],
+        "file": __logs[2],
         "parameters": {"execution_extraction": "leading_type"},
         "alignment": {"align": False, "repeat": None},
     },
     "expl": {
         "mode": None,
-        "file": __logs[7],
+        "file": __logs[6],
         "alignment": {"align": True, "repeat": True},
     },
     "eval": {
         "mode": 9,
-        "file": __logs[7],
-        "file_eval": __logs[3],
+        "file": __logs[6],
+        "file_eval": __logs[2],
         "alignment": {"align": False, "repeat": None},
     },
     "publ": {
         "mode": 9,
-        "file": __logs[5],
-        "file_eval": __logs[7],
+        "file": __logs[4],
+        "file_eval": __logs[6],
         "alignment": {"align": True, "repeat": True},
     },
 }
